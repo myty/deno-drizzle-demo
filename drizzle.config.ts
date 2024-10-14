e0c5fb143473887244a8102068d98d7a00cdb51e
Michael Tyson <myty@users.noreply.github.com>
2024-10-14 13:44:05 -0400

    update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./src/db/schema.ts",
+  schema: "src/db/models/*.ts",
   out: "./drizzle",
   dbCredentials: {
     url: Deno.env.get("DATABASE_URL") ?? "",
